14538bf6e57980dbef2e80a11c2d1e6410ecacaa
Saurabh Bathe <sbathe@hortonworks.com>
2017-07-14 12:32:52 +0530

    now we can handle a list of services and components to restart

--- restart_services.py
+++ restart_services.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-import argparse, failhadoop
+import argparse, failhadoop, sys
 parser = argparse.ArgumentParser()
 parser.add_argument("-v", action="store_true", default=False, dest='verbose',
                     help="increase output verbosity")
@@ -12,15 +12,26 @@ parser.add_argument("--component", action="store", dest='component',
 args = parser.parse_args()
 
 config = failhadoop.utils.load_config(args)
-service = args.service
-component = args.component
+service = args.service.split(',')
+component = args.component.split(',')
 config['service'] = service
 config['component'] = component
 
 s = failhadoop.ambari_helpers.setup_ambari_session(config)
 cluster = failhadoop.ambari_helpers.get_clusters(config,s)
-r = failhadoop.ambari_helpers.restart_services_in_bulk(config, cluster, s, service, component)
-if r.status_code >= 200 and r.status_code <= 299:
+
+# Either the script of the base function we call needs to be modified to
+# support passing a list of services and components to restart
+# What is done here is a band-aid
+
+def restart(config, cluster, s, service, component):
+  r = failhadoop.ambari_helpers.restart_services_in_bulk(config, cluster, s, service, component)
+  if r.status_code >= 200 and r.status_code <= 299:
     print("Services restarted successfully, track the progress at:\n\t {0}".format(r.json()['href']))
-else:
+  else:
     print("failed. Check error {0}".format(r.json()))
+
+for srv in service:
+    for comp in component:
+        restart(config, cluster, s, srv, comp)
+
