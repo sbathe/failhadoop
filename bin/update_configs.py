1edd346dedc99c1cbda0d8059c5ec1348b7a530a
Saurabh Bathe <sbathe@hortonworks.com>
2017-08-21 16:36:47 +0530

    fix for Issue #14

--- bin/update_configs.py
+++ bin/update_configs.py
@@ -14,24 +14,26 @@ config = failhadoop.utils.load_config(args)
 if args.verbose:
     print(json.dumps(config,indent=2))
 testconfig = json.load(open(args.testconfig))
-element = testconfig['configs']['element']
-if args.verbose:
-    print(element)
 s = failhadoop.ambari_helpers.setup_ambari_session(config)
 cluster = config['cluster'] if 'cluster' in config.keys() else failhadoop.ambari_helpers.get_clusters(config,s)
 if args.verbose:
     print(cluster)
-cur_tag = failhadoop.ambari_helpers.get_current_config_tag(config, cluster, s, element)
-cur_config = failhadoop.ambari_helpers.get_tagged_config(config, cluster, s,
-                                                         element, cur_tag)
-new_config = failhadoop.ambari_helpers.get_tagged_config(config, cluster, s,
-                                                         element, cur_tag)
-new_config['properties'].update(testconfig['configs']['properties'])
-r = failhadoop.ambari_helpers.update_component_config(config, cluster, s, element,
-                        new_config )
-if not (r.status_code >= 200 and r.status_code <= 299):
-    print("Updating configurations failed. The error returned is:\n{0}".format(json.dumps(r.json(),indent=2)))
-    sys.exit(2)
+
+for i in testconfig['configs']:
+    element = i['element']
+    if args.verbose:
+        print(element)
+    cur_tag = failhadoop.ambari_helpers.get_current_config_tag(config, cluster, s, element)
+    cur_config = failhadoop.ambari_helpers.get_tagged_config(config, cluster, s,
+                                                             element, cur_tag)
+    new_config = failhadoop.ambari_helpers.get_tagged_config(config, cluster, s,
+                                                             element, cur_tag)
+    new_config['properties'].update(i['properties'])
+    r = failhadoop.ambari_helpers.update_component_config(config, cluster, s, element,
+                            new_config )
+    if not (r.status_code >= 200 and r.status_code <= 299):
+        print("Updating configurations failed. The error returned is:\n{0}".format(json.dumps(r.json(),indent=2)))
+        sys.exit(2)
 
 r = failhadoop.ambari_helpers.restart_stale_configs(config, cluster, s)
 if not (r.status_code >= 200 and r.status_code <= 299):
