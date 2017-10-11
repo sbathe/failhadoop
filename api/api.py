d6641eaf1ad463a1b3df719a1d09e784441074f8
Saurabh Bathe <sbathe@hortonworks.com>
2017-10-11 15:38:54 +0530

    Fixed #19

--- api/api.py
+++ api/api.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/local/bin/python3
 
 import os, sys, json, flask, requests
 import failhadoop
@@ -38,6 +38,25 @@ def run_random_failure_on_cluster(config, cluster):
     cmd = ['fail.py',
            '-c','{0}/{1}.json'.format(d,config),'-i','{0}/inventory/'.format(d),'--testcase-root',
            '{0}'.format(flask_conf['testcase_root']),'-v','--random',
+           '--dry-run']
+    p = subprocess.Popen(cmd,stdout =
+                         subprocess.PIPE,stderr=subprocess.PIPE,stdin=subprocess.PIPE)
+    out, err = p.communicate()
+    return out
+
+@app.route(base_uri + '/<config>/<cluster>/<service>/<testnumber>', methods = ['GET'])
+def run_failure_on_cluster(config, cluster, service, testnumber):
+    # ADD processing for url inputs
+    # Check if the config exists in the config root
+    d = flask_conf['failhadoop_config_root']
+    full_path = os.path.join(d, config + '.json')
+    if not os.path.exists(full_path):
+        return "Required config file {0} in the URL is not on this server".format(full_path), 500
+
+    cmd = ['fail.py',
+           '-c','{0}/{1}.json'.format(d,config),'-i','{0}/inventory/'.format(d),'--testcase-root',
+           '{0}'.format(flask_conf['testcase_root']),'-v','--service',service,
+           '--testnumber', testnumber,
            '--dry-run']
     p = subprocess.Popen(cmd,stdout =
                          subprocess.PIPE,stderr=subprocess.PIPE,stdin=subprocess.PIPE)
